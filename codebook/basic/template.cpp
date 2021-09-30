ddb119396b39adedf54091f652a567a4266ec3ac
Adam7066 <adam20001002@gmail.com>
2021-09-30 22:47:06 +0800

    add zeller and update default template

--- codebook/basic/template.cpp
+++ codebook/basic/template.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h>
+#define inf 0x3f3f3f3f
+#define dinf 0xc0c0c0c0
+#define jizz ios::sync_with_stdio(0), cin.tie(0)
 #define EB emplace_back
 #define MP make_pair
 #define X first
@@ -7,9 +10,3 @@ using namespace std;
 using ll = long long;
 using llu = long long unsigned;
 using pii = pair<int, int>;
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    return 0;
-}

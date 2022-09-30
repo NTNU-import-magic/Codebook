63c119865ec86e596e1aabbe1c3c49f49d72bf6d
SubaRya <index20010928@gmail.com>
2022-09-30 23:47:52 +0800

    add sort_unique and get_pos definition

--- codebook/basic/template.cpp
+++ codebook/basic/template.cpp
@@ -2,11 +2,6 @@
 #define inf 0x3f3f3f3f
 #define dinf 0xc0c0c0c0
 #define jizz ios::sync_with_stdio(0), cin.tie(0)
-#define EB emplace_back
-#define MP make_pair
-#define X first
-#define Y second
+#define SORT_UNIQUE(c) (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define GET_POS(c,x) (lower_bound(c.begin(), c.end(),x)-c.begin())
 using namespace std;
-using ll = long long;
-using llu = long long unsigned;
-using pii = pair<int, int>;

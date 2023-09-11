1b3234d83ff1459f27fa09205178fab72e44ff59
KutsunaSubaRya <index20010928@gmail.com>
2023-09-11 17:38:21 +0800

    feat: add dsu union by size

--- codebook/data_structure/Disjoint_Set.cpp
+++ codebook/data_structure/Disjoint_Set.cpp
@@ -18,3 +18,23 @@ struct disjointset { // union by rank
         }
     }
 };
+struct dsu { // union by size
+    int f[MAX], sz[MAX];
+    void init(int N) {
+        for (int i=0; i<N; i++) sz[i] = 1;
+        for (int i=0; i<N; i++) f[i] = i;
+    }
+    int find(int val) {
+        return f[val]==val?val:f[val]=find(f[val]);
+    }
+    bool same(int a, int b) {
+        return find(a) == find(b);
+    }
+    void Union(int l, int r) {
+        if(!same(l,r)){
+            int tmpRightP = f[r];
+            f[f[r]] = f[l];
+            sz[f[l]] += sz[tmpRightP];
+        }
+    }
+};
